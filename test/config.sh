269126d744b42b8cd0703f5a53f37602bd4280e9
Frankeo <ingfrancisco.moreno1@gmail.com>
2024-02-19 22:48:31 +0100

    testing filebrowser docker

--- test/config.sh
+++ test/config.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-## Script variables ##
-REPO_URL=https://github.com/Frankeo/home-server.git
-REPO_PATH=./home-server/src/playbook.yml
-## Install Git ##
+
+## Install Git and Ansible ##
 apt-get update
 apt-get install -y git ansible
+
 ## Clone Git repo ##
-git clone $REPO_URL
+git clone https://github.com/Frankeo/home-server.git
+
 ## Execute playbook ##
-ansible-playbook $REPO_PATH
+ansible-playbook ./home-server/src/playbook.yml
+
 ## Finish and clean installation ##
 apt-get autoclean -y
 apt-get autoremove -y
