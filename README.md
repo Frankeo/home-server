5c4664c3236a8348c0fb83a768466198933a054f
Frankeo <ingfrancisco.moreno1@gmail.com>
2024-10-03 18:18:43 +0200

    testing from scratch

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 Tested over Ubuntu Server 22.04 LTS, Execute:
 
 ```bash
-bash <(wget -qO- https://raw.githubusercontent.com/Frankeo/home-server/refs/heads/main/config.sh)
+bash <(wget -qO- https://raw.githubusercontent.com/Frankeo/home-server/refs/heads/feature/testing-docker-compose/config.sh)
 ```
---
+++ src/nginx-proxy-manager/init-hosts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## Create Certificate
+sudo ./nginx_proxy_manager_cli.sh --generate-cert home-server-flme.duckdns.org admin@admin.com TOKEN_DNS
+sudo ./nginx_proxy_manager_cli.sh --generate-cert *.home-server-flme.duckdns.org admin@admin.com TOKEN_DNS
+
+## Create Hosts
+sudo ./nginx_proxy_manager_cli.sh -d node.home-server-flme.duckdns.org -i flame -p 4000 -f http -t 2 -c true
+sudo ./nginx_proxy_manager_cli.sh -d home-server-flme.duckdns.org -i nginx-proxy-manager -p 81 -f http -t 2 -c true
