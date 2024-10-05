830887ecb806b3b392b1ca76f120fa2feb72603b
Frankeo <ingfrancisco.moreno1@gmail.com>
2024-10-05 13:35:11 +0200

    add default values for nginx proxy manager

--- src/nginx-proxy-manager/init-hosts.sh
+++ src/nginx-proxy-manager/init-hosts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ## Create Certificate
-sudo ./nginx_proxy_manager_cli.sh --generate-cert home-server-flme.duckdns.org admin@admin.com TOKEN_DNS
-sudo ./nginx_proxy_manager_cli.sh --generate-cert *.home-server-flme.duckdns.org admin@admin.com TOKEN_DNS
+sudo ./nginx_proxy_manager_cli.sh --generate-cert home-server-flme.duckdns.org admin@example.com TOKEN_DNS
+sudo ./nginx_proxy_manager_cli.sh --generate-cert *.home-server-flme.duckdns.org admin@example.com TOKEN_DNS
 
 ## Create Hosts
 sudo ./nginx_proxy_manager_cli.sh -d node.home-server-flme.duckdns.org -i flame -p 4000 -f http -t 2 -c true
